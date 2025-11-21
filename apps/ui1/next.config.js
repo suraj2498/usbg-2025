dc1cf63229d65b3c28961b66d6b56368b093b5fe
suraj kumarwq <srjkmr1024@gmail.com>
2025-11-20 20:04:54 -0500

    chore: skip eslint on build

--- apps/ui1/next.config.js
+++ apps/ui1/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: ['@monorepo/ui', '@monorepo/forms'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   env: {
     API_URL: process.env.API_URL || 'http://localhost:4000',
   },
